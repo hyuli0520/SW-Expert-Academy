22cdf96de9d947cff83bbc8287d8c2272d1d3e6e
minhyeok <79748783+hyuli0520@users.noreply.github.com>
2026-02-04 09:19:56 +0900

    Create 16268. 풍선팡2.py

---
+++ SWEA/16268. 풍선팡2.py
@@ -0,0 +1,29 @@
+import sys
+sys.stdin = open("input.txt", "r")
+
+T = int(input())
+
+for test_case in range(1, T + 1):
+    N, M = map(int, input().split()) # N = 열, M = 행
+    matrix = [list(map(int, input().split())) for _ in range(N)]
+
+    dx = [0, 0, -1, 1]  # 상, 하, 좌, 우
+    dy = [1, -1, 0, 0]  # 상, 하, 좌, 우
+
+    ans = 0
+    for i in range(N):
+        for j in range(M): # 행 -> 열 순으로 확인
+            tmp = matrix[i][j] # matrix의 i, j 번째에 있는 수를 tmp에 저장
+
+            high = matrix[i][j]
+            for k in range(4): # (i, j)를 기준으로 상, 하, 좌, 우를 dist 만큼 탐색
+                ni = i + dx[k]
+                nj = j + dy[k]
+
+                if 0 <= ni < N and 0 <= nj < M:
+                    high += matrix[ni][nj]
+
+            if ans < high:
+                ans = high
+
+    print(f"#{test_case} {ans}")
